4a8894cabeecfeee455258257ce0bba3ac68b527
Wojciech Krzystek <wojciech.krzystek@gmail.com>
2013-05-25 20:45:06 +0200

    CrudRepository --> JpaRepository + ID type fix

--- src/main/java/pl/edu/agh/db2/northwind/dao/SupplierRepository.java
+++ src/main/java/pl/edu/agh/db2/northwind/dao/SupplierRepository.java
@@ -1,9 +1,9 @@
 package pl.edu.agh.db2.northwind.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.edu.agh.db2.northwind.model.Supplier;
 
 @Repository
-public interface SupplierRepository extends CrudRepository<Supplier, Long> {
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
 }
