f1ab0faaacdebc360a142dbefdcdbc4655c79e3c
Grzegorz Wilaszek <wilaszekg@gmail.com>
2013-05-26 20:06:29 +0200

    sample entities OXM loading -- problem with Spring & JPA & caching combo
    Exception in thread "main" org.hibernate.LazyInitializationException: could not initialize proxy - no Session
    (problematic code commented out with "FIXME" comment)

--- src/main/java/pl/edu/agh/db2/northwind/demo/MainApp.java
+++ src/main/java/pl/edu/agh/db2/northwind/demo/MainApp.java
@@ -4,14 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
-import pl.edu.agh.db2.northwind.dao.SupplierRepository;
+import pl.edu.agh.db2.northwind.dao.*;
+import pl.edu.agh.db2.northwind.model.Category;
+import pl.edu.agh.db2.northwind.model.Product;
+import pl.edu.agh.db2.northwind.model.Supplier;
 import pl.edu.agh.db2.northwind.oxm.XmlConverter;
+import pl.edu.agh.db2.northwind.oxm.holders.ListHolder;
+
+import java.util.List;
 
 @Component
 public class MainApp {
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Autowired
-	private SupplierRepository dao;
+	private CustomerRepository customerRepository;
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Autowired
+	private OrderDetailRepository orderDetailRepository;
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private ShipperRepository shipperRepository;
+
+	@Autowired
+	private SupplierRepository supplierRepository;
 
 	@Autowired
 	private XmlConverter converter;
@@ -24,5 +50,19 @@ public class MainApp {
 	}
 
 	private void start(String[] args) {
+		List<Category> unmarshalledCategories = ((ListHolder<Category>) converter.loadFromXml("categories.xml")).getValues();
+		List<Supplier> unmarshalledSuppliers = ((ListHolder<Supplier>) converter.loadFromXml("suppliers.xml")).getValues();
+		List<Product> unmarshalledProducts = ((ListHolder<Product>) converter.loadFromXml("products.xml")).getValues();
+
+		categoryRepository.save(unmarshalledCategories);
+		supplierRepository.save(unmarshalledSuppliers);
+		productRepository.saveAll(unmarshalledProducts);
+
+		System.out.println(productRepository.findOne(12));
+		System.out.println(productRepository.findOne(12).getProductName());
+		System.out.println(productRepository.findOne(12).getSupplierId());
+		System.out.println(supplierRepository.findOne(5).getCompanyName());
+		// FIXME: causes "could not initialize proxy - no Session " problem
+//		System.out.println(productRepository.findOne(12).getSupplier().getCompanyName());
 	}
 }
